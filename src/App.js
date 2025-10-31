89c22c67b9fcb8db626dc83cce3bf1778fe86faf
Sondos Aamer <sondosaamer12@example.com>
2025-10-31 15:33:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
       <Route path='/about' element={<About/>}/>
       <Route path='/services' element={<Services/>}/>
       <Route path='/*' element={<Home/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
     </Routes>
     <Footer/>
